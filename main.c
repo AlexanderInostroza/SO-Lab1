81857a6ab9aafbf2b85584ea38eeb5472fb13152
GabrielLeight <107009481+GabrielLeight@users.noreply.github.com>
2023-08-07 21:52:06 -0400

    se vienen cositas 2

--- main.c
+++ main.c
@@ -8,12 +8,30 @@
 int main(){
     DIR *d;
     struct dirent *dir;
+    FILE* pointer;
     d = opendir("./Archivos_de_prueba");
+    char prueba[70] = "Archivos_de_prueba/\0";
+
     if (d){
         while ((dir = readdir(d)) != NULL){
-            printf("%s\n", dir->d_name);
+            
+            if (strstr(dir->d_name,".txt") != NULL){
+                memcpy(prueba,"Archivos_de_prueba/\0",strlen(prueba));
+                printf("%s\n", prueba);
+                strcat(prueba,dir->d_name);
+                printf("%s\n", prueba);
+                pointer = fopen(prueba,"r");
+                if (pointer == NULL){
+                    printf("No existe.");
+                }
+                
+                
+                
+            }
         }
+        fclose(pointer);
         closedir(d);
     }
+
     return 0;
 }
